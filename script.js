b221c11f2b1a9348e80f9ceb41b0e9c3295c2722
edjozero <ej21serafin@gmail.com>
2026-03-23 22:59:22 -0700

    Adjust lightbox html element, fix closing lightbox listener, adjust lightbox listener to open clicked image

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const galleryItems = document.querySelectorAll('.gallery-item');
 const lightbox = document.querySelector('.lightbox');
-const closeBtn = document.querySelector('.close-btn');
+const closeBtn = document.querySelector('#close-btn');
+const lightboxImage = document.getElementById('lightbox-image');
 
 function closeLightbox() {
     lightbox.style.display = "none";
 }
 
+closeBtn.addEventListener('click', closeLightbox);
+
 galleryItems.forEach(item => {
     item.addEventListener('click', () => {
+        lightboxImage.src = item.src;
+
+        console.log(lightboxImage.src);
+        
         lightbox.style.display = "flex";
     });
 });
